0b4b2f782ae30d5ee8b34e38cc4d74337fb4afa0
unknown <j.madden@u-bl-eng-bs-w2k.prd.cs.int>
2015-06-16 23:39:11 +0100

    handle excluding certain directories to avoid unwanted large files

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,13 @@ var fileList = require('./file-list.json');
 fileList.files = _.map(fileList.files, function(filename) {
   return fileList.base + '/' + filename;
 });
+fileList.exclude =  _.map(fileList.exclude, function(filename) {
+  return '!' + fileList.base + '/' + filename;
+});
+fileList.files = fileList.exclude.concat(fileList.files);
 
 gulp.task('copy', function(){
   gulp.src(fileList.files, {base: fileList.base})
     .pipe(gulp.dest('dist'));
 });
+
